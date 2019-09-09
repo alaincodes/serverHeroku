8940e83f6430ca66ba4f3c79068ecb2e4b4551d1
Alain Tran <alaint77@gmail.com>
2019-09-09 20:43:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const bodyParser = require("body-parser");
 
-const Note = require('./models/note')
+const Note = require("./models/note");
 
-app.use(bodyParser.json())
-const cors = require('cors')
+app.use(bodyParser.json());
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-app.use(requestLogger)
+app.use(requestLogger);
 
 let notes = [
   {
@@ -39,59 +39,59 @@ let notes = [
     date: "2019-05-30T19:20:14.298Z",
     important: true
   }
-]
+];
 
-app.use(express.static('build'))
+app.use(express.static("build"));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
   if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
+    return response.status(400).json({ error: "content missing" });
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-  })
+    date: new Date()
+  });
 
   note.save().then(savedNote => {
-    response.json(savedNote.toJSON())
-  })
-})
+    response.json(savedNote.toJSON());
+  });
+});
 
-app.get('/api/notes', (request, response) => {
+app.get("/api/notes", (request, response) => {
   Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
+    response.json(notes);
+  });
+});
 
-app.get('/api/notes/:id', (request, response) => {
+app.get("/api/notes/:id", (request, response) => {
   Note.findById(request.params.id).then(note => {
-    response.json(note.toJSON())
-  })
-})
+    response.json(note.toJSON());
+  });
+});
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
